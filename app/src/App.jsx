a53ed26fde829a93975a97bb5079d66850b63f4e
LOZEST <lulubisiaux@gmail.com>
2026-05-04 22:19:11 +0200

    Adopter une direction visuelle type hypercar premium

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,35 +1,17 @@
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
-import { AnimatePresence } from 'framer-motion'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Nav from './components/Nav'
 import Footer from './components/Footer'
 import Home from './pages/Home'
-import Equipe from './pages/Equipe'
-import Conception from './pages/Conception'
-import Vision from './pages/Vision'
-import Models from './pages/Models'
 import './App.css'
-
-function AnimatedRoutes() {
-  const location = useLocation()
-  return (
-    <AnimatePresence mode="wait">
-      <Routes location={location} key={location.pathname}>
-        <Route path="/" element={<Home />} />
-        <Route path="/equipe" element={<Equipe />} />
-        <Route path="/conception" element={<Conception />} />
-        <Route path="/vision" element={<Vision />} />
-        <Route path="/modeles" element={<Models />} />
-      </Routes>
-    </AnimatePresence>
-  )
-}
 
 export default function App() {
   return (
     <BrowserRouter>
-      <div className="page">
+      <div className='page'>
         <Nav />
-        <AnimatedRoutes />
+        <Routes>
+          <Route path='*' element={<Home />} />
+        </Routes>
         <Footer />
       </div>
     </BrowserRouter>
